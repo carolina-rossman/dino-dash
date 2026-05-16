2f9a1fa0dc610aac0e660f1302904d57a0fe4e51
carolina-rossman <rossman5@illinois.edu>
2026-05-16 12:15:37 -0400

    adding comments to main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import start_screen
-
+# loads start_screen.py, which runs instructions screen to powers_screen then finally game.py
+# decided to do it this way to it's more intuative what to run 
 def main(): 
     start_screen.main()
 
