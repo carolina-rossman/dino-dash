c4589592b454976c328200ca4734e8bd8d04591b
a-tran69 <atran69@illinois.edu>
2026-05-17 21:36:19 -0500

    comments added to death and end screens

--- src/death_screen.py
+++ src/death_screen.py
@@ -8,19 +8,24 @@ class Display:
     def __init__(self):
         pygame.init()
         pygame.freetype.init()
+        # setting screen size and creating the window
         self.window = pygame.display.set_mode((800, 600))
         self.font = pygame.freetype.SysFont("Arial", 65, bold=True)
         self.background = pygame.Surface(self.window.get_size())
+        # background color
         self.red = [128, 0, 0]
         self.background.fill(self.red)
+        # setting the position of the buttons
         self.restart_button = pygame.Rect(80, 500, 200, 50)
         self.quit_button = pygame.Rect(520, 500, 200, 50)
         self.window.blit(self.background, (0, 0))
+        # creating the text and setting its position
         text_surface, rect = self.font.render("You Died")
         self.window.blit(text_surface, (250, 250))
         pygame.display.flip()
 
     def create_buttons(self):
+        # creates the buttons
         pygame.draw.rect(self.window, (0, 200, 0), self.restart_button)
         pygame.draw.rect(self.window, (200, 0, 0), self.quit_button)
         self.button_font = pygame.font.SysFont("Arial", 20, bold=True)
@@ -37,15 +42,19 @@ class Display:
             self.create_buttons()
             pygame.display.update()
             for event in pygame.event.get():
+                # when you quit the screen, everything else quits too
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    print("Restart!")
+                    # when you press the restart button with your mouse you are taken back the the beginning of game.py
                     if self.restart_button.collidepoint(event.pos):
+                        print("Restart!")
                         new_screen = game.main()
                         new_screen.restart
+                    # when you press the quit button with your mouse, everything else quits too
                     if self.quit_button.collidepoint(event.pos):
+                        print("Quqit")
                         pygame.quit()
                         sys.exit()
             pygame.display.update()
--- src/end_screen.py
+++ src/end_screen.py
@@ -8,14 +8,18 @@ class Display:
     def __init__(self):
         pygame.init()
         pygame.freetype.init()
+        # setting screen size and creating the window
         self.window = pygame.display.set_mode((800, 600))
         self.font = pygame.freetype.SysFont("Arial", 65, bold=True)
         self.background = pygame.Surface(self.window.get_size())
+        # background color
         self.blue = [100, 149, 237]
         self.background.fill(self.blue)
+        # setting the position of the buttons
         self.play_again_button = pygame.Rect(80, 500, 200, 50)
         self.quit_button = pygame.Rect(520, 500, 200, 50)
         self.window.blit(self.background, (0, 0))
+        # creating the text and setting its position
         text_one, rect = self.font.render("Congrats!")
         text_two, rect = self.font.render("You Escaped Jurassic Park!")
         self.window.blit(text_one, (250, 200))
@@ -23,6 +27,7 @@ class Display:
         pygame.display.flip()
 
     def create_buttons(self):
+        # creates the buttons
         pygame.draw.rect(self.window, (0, 200, 0), self.play_again_button)
         pygame.draw.rect(self.window, (200, 0, 0), self.quit_button)
         self.button_font = pygame.font.SysFont("Arial", 20, bold=True)
@@ -41,15 +46,20 @@ class Display:
             self.create_buttons()
             pygame.display.update()
             for event in pygame.event.get():
+                # when you quit the screen, everything else quits too
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    print("Play Again!")
+                    # when you press the play again button with your mouse you are taken back the the beginning of game.py
                     if self.play_again_button.collidepoint(event.pos):
+                        print("Play Again!")
                         new_screen = game.main()
                         new_screen.play_again
+                    # when you press the quit button with your mouse, everything else quits too
                     if self.quit_button.collidepoint(event.pos):
+                        print("Quit")
                         pygame.quit()
                         sys.exit()
             pygame.display.update()
